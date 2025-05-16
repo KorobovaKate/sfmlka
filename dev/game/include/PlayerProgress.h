ae7a02065f01a1733276c11fb868b269bbe20733
KorobovaKate <kat3kor@yandex.ru>
2025-05-16 09:13:09 +0300

    перенос playerProgress

--- dev/game/include/PlayerProgress.h
+++ dev/game/include/PlayerProgress.h
@@ -10,34 +10,8 @@ public:
 	std::string Name;
 	int Score;
 	int CurrentLevel;
-	bool ReadFromFile(std::string fileName)
-	{
-		std::ifstream file;
-		file.open(fileName + ".txt");
-		if (file.is_open() == false)
-		{
-			std::cout << "не удалось открыть файл" << std::endl;
-			return false;
-		}
-		file >> Name;
-		file >> Score;
-		file >> CurrentLevel;
-		file.close();
-		return true;
-	}
-	bool WriteToFile()
-	{
-		std::ofstream file; //создаю переменную "указывающюю" на файл
-		file.open(Name + ".txt");
-		if (file.is_open() == false)
-			return false;
-
-		file << Name << std::endl;
-		file << Score << std::endl;
-		file << CurrentLevel << std::endl;
-		file.close();
-		return true;
-	}
+	bool ReadFromFile(std::string fileName);
+	bool WriteToFile();
 };
 
 
--- dev/game/src/PlayerProgress.cpp
+++ dev/game/src/PlayerProgress.cpp
@@ -1,3 +1,34 @@
 #include <PlayerProgress.h>
 
 PlayerProgress CurrentPlayerProgress;
+
+
+bool PlayerProgress::ReadFromFile(std::string fileName)
+{
+	std::ifstream file;
+	file.open(fileName + ".txt");
+	if (file.is_open() == false)
+	{
+		std::cout << "не удалось открыть файл" << std::endl;
+		return false;
+	}
+	file >> Name;
+	file >> Score;
+	file >> CurrentLevel;
+	file.close();
+	return true;
+}
+
+bool PlayerProgress::WriteToFile()
+{
+	std::ofstream file; //создаю переменную "указывающюю" на файл
+	file.open(Name + ".txt");
+	if (file.is_open() == false)
+		return false;
+
+	file << Name << std::endl;
+	file << Score << std::endl;
+	file << CurrentLevel << std::endl;
+	file.close();
+	return true;
+}
